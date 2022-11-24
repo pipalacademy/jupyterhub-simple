85f22430aa6717e236b61971657aea8f32fd7f9b
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-11-24 11:02:39 +0530

    add-users.py: Set shell to /bin/bash by default
    /bin/bash is a more practical default for shell than the system default
    (/bin/sh).
    See issue: https://github.com/pipalacademy/jupyterhub-simple/issues/1

--- src/add-users.py
+++ src/add-users.py
@@ -4,6 +4,8 @@ import os
 
 usersfile = sys.argv[1]
 
+default_shell = "/bin/bash"
+
 if not os.path.exists(usersfile):
     print(usersfile, "No users.txt file found. Ignoring...", file=sys.stderr)
     sys.exit(0)
@@ -11,6 +13,6 @@ if not os.path.exists(usersfile):
 for line in open(usersfile):
     username, pw = line.strip().split(":", 1)
     cryptpw = crypt.crypt(pw)
-    os.system("useradd -m -p '{}' {}".format(cryptpw, username))
+    os.system(f"useradd -m -s '{default_shell}' -p '{cryptpw}' {username}")
     os.system("chown -R {0}:{0} /home/{0}".format(username))
     print("Added user", username)
